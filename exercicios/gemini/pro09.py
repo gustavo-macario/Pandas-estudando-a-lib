1d12cbd7a0f5ea3c695198e3255cc4ed8d252c30
skazy737 <macario737@outlook.com>
2026-07-06 20:59:34 -0300

    ex

--- exercicios/gemini/pro09.py
+++ exercicios/gemini/pro09.py
@@ -42,5 +42,83 @@ def desconto(p):
 df_3['desconto']= df_3['vlProduto'].apply(desconto)
 media = df_3['desconto'].mean()
 media
-# %%
 
+
+
+# %%
+# 4 Crie uma tabela resumo mostrando, para cada Sistema de Origem (DescSistemaOrigem), três métricas simultâneas:
+# A quantidade de clientes únicos que compraram por lá.
+# O total de itens comprados.
+# O ticket médio.
+df_4 = df_transacoes.copy()
+df_4 = df_4.merge(right=df_transacao_produto, how='inner', on='IdTransacao')
+df_4['total'] = df_4['QtdeProduto'] * df_4['vlProduto']
+resumo = df_4.groupby('DescSistemaOrigem').agg(
+   clientes_unicos=('IdCliente', 'nunique'),
+   total_itens=('QtdeProduto', 'sum'),
+   ticket_medio=('total', 'mean')
+)
+resumo
+
+
+# %%
+# 5 Calcule o valor médio, em reais (R$), que os clientes gastam especificamente na sua terceira compra na loja.
+# (Desconsidere clientes que compraram menos de 3 vezes).
+df_5 = df_transacoes.copy()
+df_5 = df_5.merge(right=df_transacao_produto, how='inner', on='IdTransacao')
+df_5['DtCriacao'] = pd.to_datetime(df_5['DtCriacao'])
+df_5 = df_5.sort_values(by='DtCriacao')
+df_5['numeracao_compras'] = df_5.groupby('IdCliente').cumcount()
+df_5 = df_5[df_5['numeracao_compras'] == 2]
+df_5['total'] = df_5['QtdeProduto'] * df_5['vlProduto']
+media = df_5['total'].mean()
+media
+
+
+
+# %%
+# 6 Isole todos os produtos cujo nome comece com a letra "C" (independentemente de ser maiúscula ou minúscula).
+# Qual foi a receita financeira histórica total gerada apenas pela venda desses produtos específicos?
+df_6 = df_transacao_produto.copy()
+df_6 = df_6.merge(right=df_produtos, how='inner', on='IdProduto')
+filtro_letra_c = df_6['DescNomeProduto'].str.lower().str.startswith('c')
+df_6 = df_6[filtro_letra_c]
+df_6['faturamento'] = df_6['QtdeProduto'] * df_6['vlProduto']
+receita_total = df_6['faturamento'].sum()
+receita_total
+
+
+# %%
+# 7 Extraia uma lista com os IdCliente das pessoas que já compraram itens da categoria "cajado", mas que nunca, 
+# em nenhum momento, compraram itens da categoria "espada".
+df_7 = df_produtos
+df_7 = df_7.merge(right=df_transacao_produto, how='inner', on='IdProduto')
+df_7 = df_7.merge(right=df_transacoes, how='inner', on='IdTransacao')
+clientes_cajado = df_7[df_7['DescNomeProduto'] == 'cajado']['IdCliente'].unique()
+clientes_espada = df_7[df_7['DescNomeProduto'] == 'espada']['IdCliente'].unique()
+clientes_filtrados = clientes_cajado[~pd.Series(clientes_cajado).isin(clientes_espada)]
+clientes_filtrados
+
+
+# %%
+# 8 Quantos clientes fizeram a sua primeira compra no final de semana (sábado ou domingo) em um prazo menor
+# ou igual a 30 dias após terem criado a conta (DtCriacao do cliente)?
+df_8 = df_clientes.copy()
+df_8 = df_8.merge(right=df_transacoes, how='inner', on='IdCliente')
+df_8['DtCriacao_x'] = pd.to_datetime(df_8['DtCriacao_x']) 
+df_8['DtCriacao_y'] = pd.to_datetime(df_8['DtCriacao_y']) 
+df_8 = df_8.sort_values(by='DtCriacao_y')
+df_8['num_compra'] = df_8.groupby('IdCliente').cumcount()
+df_primeira_compra = df_8[df_8['num_compra'] == 0].copy()
+
+df_primeira_compra['dias'] = (df_primeira_compra['DtCriacao_y'] - df_primeira_compra['DtCriacao_x']).dt.days
+df_primeira_compra = df_primeira_compra[df_primeira_compra['dias'] <= 30]
+
+df_primeira_compra['dia_semana'] = df_primeira_compra['DtCriacao_y'].dt.weekday
+
+df_final = df_primeira_compra[df_primeira_compra['dia_semana'] >= 5]
+
+quantidade_clientes = df_final.shape[0]
+
+print(f"Total de clientes que atenderam às condições: {quantidade_clientes}")
+# %%
