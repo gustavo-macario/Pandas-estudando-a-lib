c366dc624b9272ddfeaa90ac18177293160967fe
skazy737 <macario737@outlook.com>
2026-07-13 21:11:10 -0300

    ex

--- exercicios/gemini/pro11.py
+++ exercicios/gemini/pro11.py
@@ -139,6 +139,7 @@ processadores = df_10[df_10['categoria'] == 'Processador']
 gpus = df_10[df_10['categoria'] == 'Placa de Vídeo']
 placas_mae = df_10[df_10['categoria'] == 'Placa Mãe']
 
+
 maior_score_encontrado = 0
 melhor_combinacao = {}
 
@@ -158,5 +159,140 @@ for idx_p, proc in processadores.iterrows():
                 }
 
 print(melhor_combinacao)
+
+
+
+# %%
+# 11 Ter um processador incrível e uma placa de vídeo ruim gera "gargalo". Escreva um algoritmo que encontre a combinação de 1 Processador
+# e 1 Placa de Vídeo (ambos em estoque) onde a diferença de score_desempenho entre eles seja menor ou igual a 5 pontos. 
+# Dentre as combinações que passarem nessa regra, exiba a que tiver o menor custo total.
+df_11 = df_estoque_hardware.copy()
+df_11 = df_11[df_11['estoque'] > 0]
+df_11 = df_11.sort_values(by='score_desempenho', ascending=False)
+
+processadores = df_11[df_11['categoria'] == 'Processador']
+placas_de_video = df_11[df_11['categoria'] == 'Placa de Vídeo']
+
+combinacao = []
+score = []
+menor_custo_salvo = 9999999
+
+for ip, p in processadores.iterrows():
+    for ipv, pv in placas_de_video.iterrows():
+        score = abs(p['score_desempenho'] - pv['score_desempenho'])
+        custo_total = p['preco'] + pv['preco']
+        
+        if score <= 5 and custo_total < menor_custo_salvo:
+            combinacao = {
+                'Processador' : p['modelo'],
+                'Placa de Video' : pv['modelo'],
+                'Custo Total' : custo_total
+            }
+
+            menor_custo_salvo = custo_total
+combinacao
+
+
+# %%
+# 12 Crie um algoritmo para descobrir qual é o "Kit Upgrade" (1 Processador + 1 Placa Mãe + 1 Memória RAM) que entrega o maior ROI
+#  (Retorno sobre Investimento).A regra é: Some os scores das 3 peças e divida pela soma dos preços. A combinação que tiver o maior
+#  resultado dessa divisão é a vencedora.
+df_12 = df_estoque_hardware.copy()
+df_12 = df_12[df_12['estoque'] > 0]
+df_12 = df_12.sort_values(by='score_desempenho', ascending=False)
+
+processador = df_12[df_12['categoria'] == 'Processador']
+placa_mae= df_12[df_12['categoria'] == 'Placa Mãe']
+memoria_ram= df_12[df_12['categoria'] == 'Memória RAM']
+
+kit_upgrade = {}
+media_pecas_recorde = 0
+
+for ip, pd in processador.iterrows():
+    for ipm, pm in placa_mae.iterrows():
+        for im, m in memoria_ram.iterrows():
+            soma_scores = pd['score_desempenho'] + pm['score_desempenho'] + m['score_desempenho']
+            soma_precos = pd['preco'] + pm['preco'] + m['preco']
+            media_pecas = soma_scores / soma_precos
+
+            if media_pecas > media_pecas_recorde:
+                kit_upgrade = {
+                    'Processador' : pd['modelo'],
+                    'Placa Mae' : pm['modelo'],
+                    'Memoria Ram' : m['modelo']
+                }
+                media_pecas_recorde = media_pecas
+kit_upgrade          
+
+
+
+# %%
+# 13 Na vida real, processadores Intel não encaixam em placas-mãe AMD.
+# Procesadores com "Core" no nome só aceitam a placa-mãe "B560M".
+# Processadores com "Ryzen" no nome só aceitam as placas "A320M" ou "X670E".
+# Faça um loop que cruze processadores e placas-mãe em estoque. Se a combinação for compatível segundo as regras acima, salve-a. 
+# No final, exiba qual é o combo compatível mais potente (maior soma de score).
+df_13 = df_estoque_hardware.copy()
+df_13 = df_13[df_13['estoque'] > 0]
+df_13 = df_13.sort_values(by='score_desempenho', ascending=False)
+
+processador = df_12[df_12['categoria'] == 'Processador']
+placa_mae = df_12[df_12['categoria'] == 'Placa Mãe']
+combo_core = {}
+combo_ryzen = {}
+score_core_recorde = 0
+score_ryzen_recorde = 0
+
+for ip, pd in processador.iterrows():
+    for ipm, pm in placa_mae.iterrows():
+        score = pd['score_desempenho'] + pm['score_desempenho']
+        if 'Core' in pd['modelo'] and 'B560M' in pm['modelo'] and score > score_core_recorde:
+            combo_core = {
+                'Processador' : pd['modelo'],
+                'Placa Mae': pm['modelo'],
+                'Score': score
+            }
+            score_core_recorde = score
+
+        if 'Ryzen' in pd['modelo'] and pm['modelo'] in ('A320M', 'X670E') and score > score_ryzen_recorde:
+            combo_ryzen = {
+                'Processador' : pd['modelo'],
+                'Placa Mae': pm['modelo'],
+                'Score': score
+            }
+            score_ryzen_recorde = score
+        
+if score_ryzen_recorde > score_core_recorde:
+            print (combo_ryzen)
+else:
+            print(combo_core)
+
+
+
+# %%
+# 14 A loja precisa fazer caixa rápido e vai vender os itens mais potentes primeiro.
+# Use um laço while. A cada rodada, encontre a peça em estoque com o maior score_desempenho, subtraia 1 do estoque dela e 
+# some o valor dela em uma variável caixa_atual. O laço deve parar exatamente quando o caixa_atual ultrapassar R$ 10.000,00. 
+# Imprima quantas peças foram vendidas no total.
+df_14 = df_estoque_hardware.copy()
+
+caixa_atual = 0
+total_vendido = 0
+
+while caixa_atual <= 10000:
+    disponiveis = df_14[df_14['estoque'] > 0]
+    
+    disponiveis = disponiveis.sort_values(by='score_desempenho', ascending=False)
+    
+    idx_campeao = disponiveis.index[0]
+
+    df_14.loc[idx_campeao, 'estoque'] -= 1
+    caixa_atual += df_14.loc[idx_campeao, 'preco']
+
+    total_vendido += 1
+
+    if caixa_atual > 10000:
+        print(total_vendido)
+        break
 
 # %%
