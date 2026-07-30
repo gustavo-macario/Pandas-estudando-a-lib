901c70ee49703b54b3e9a321aa7ca351da698446
skazy737 <macario737@outlook.com>
2026-07-29 21:04:37 -0300

    exleetcode

--- exercicios/leetcode/ex.py
+++ exercicios/leetcode/ex.py
@@ -79,3 +79,65 @@ def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
 
     second = salarios_unicos.iloc[[1]]
     return second.rename(columns={'salary': 'SecondHighestSalary'})
+
+
+# %%
+# 184. Department Highest Salary
+import pandas as pd
+
+def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    top_employees = employee.merge(right=department, how='inner', left_on='departmentId', right_on='id')
+    top_employees['max_salary'] = top_employees.groupby('departmentId')['salary'].transform('max')
+    top_employees = top_employees[top_employees['salary'] == top_employees['max_salary']]
+    top_employees = top_employees[['name_y', 'name_x', 'salary']]
+    return top_employees.rename(columns={
+        'name_y': 'Department', 
+        'name_x': 'Employee', 
+        'salary': 'Salary'
+    })
+
+# %%
+# 178. Rank Scores
+import pandas as pd
+
+def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+    scores = scores.sort_values(by='score', ascending=False)
+    scores['rank'] = scores['score'].rank(method='dense', ascending=False)
+    return scores[['score', 'rank']]
+
+# %%
+# 196. Delete Duplicate Emails
+import pandas as pd
+
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values(by='id', inplace=True)
+    person.drop_duplicates(subset=['email'], keep='first', inplace=True)
+
+# %%
+# 1795. Rearrange Products Table
+import pandas as pd
+
+def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    df = products.melt(
+        id_vars='product_id',
+        value_vars=['store1', 'store2', 'store3'],
+        var_name='store',
+        value_name='price'
+    ).dropna()
+    return df
+
+# %%
+# 1907. Count Salary Categories
+import pandas as pd
+
+def count_salary_categories(accounts: pd.DataFrame) -> pd.DataFrame:
+    low_count = len(accounts[accounts['income'] < 20000])
+    avg_count = len(accounts[(accounts['income'] >= 20000) & (accounts['income'] <= 50000)])
+    high_count = len(accounts[accounts['income'] > 50000])
+    
+    resultado = pd.DataFrame({
+        'category': ['Low Salary', 'Average Salary', 'High Salary'],
+        'accounts_count': [low_count, avg_count, high_count]
+    })
+    
+    return resultado
